569eae4411efea9ea87c474dd7a01574ef21aa1a
Florian Cassayre <florian.cassayre@gmail.com>
2021-02-18 15:12:48 +0100

    Generalization of numeric types

--- src/main/scala/competitivescala/utils/BinarySearch.scala
+++ src/main/scala/competitivescala/utils/BinarySearch.scala
@@ -2,58 +2,74 @@ package competitivescala.utils
 
 object BinarySearch {
 
-  def binarySearch[T](array: IndexedSeq[T], needle: T)(implicit ord: Ordering[T]): Option[Int] = {
-    import ord._
-    def find(start: Int, end: Int): Option[Int] = {
+  def binarySearch[N, T](mapping: N => T, first: N, last: N, needle: T)(implicit ev: Integral[N], ord: Ordering[T]): Option[N] = {
+    import ev._
+    val two = one + one
+    def find(start: N, end: N): Option[N] = {
       if(start <= end) {
-        val mid = (start + end) / 2
-        val value = array(mid)
+        val mid = (start + end) / two
+        val value = mapping(mid)
         if(value == needle) {
           Some(mid)
         } else {
-          if(value < needle) {
-            find(mid + 1, end)
+          if(ord.lt(value, needle)) {
+            find(mid + one, end)
           } else {
-            find(start, mid - 1)
+            find(start, mid - one)
           }
         }
       } else {
         None
       }
     }
-    find(0, array.size - 1)
+    find(first, last)
   }
+
+  def binarySearchArray[T](array: IndexedSeq[T], lower: Int, upper: Int, needle: T)(implicit ord: Ordering[T]): Option[Int] =
+    binarySearch(array, lower, upper, needle)
+
+  def binarySearchArray[T](array: IndexedSeq[T], needle: T)(implicit ord: Ordering[T]): Option[Int] =
+    binarySearchArray(array, 0, array.size - 1, needle)
+
 
   // ... X N N Y ... with needle = N
   //     ^_ _ _ ____ upper = f, strict = t (greatest element less than needle)
   //       ^_ _ ____ upper = f, strict = f (first occurrence of needle)
   //         ^_ ____ upper = t, strict = f (last occurrence of needle)
   //           ^____ upper = t, strict = t (least element greater than needle)
-  def binarySearchBound[T](array: IndexedSeq[T], needle: T, upper: Boolean, strict: Boolean = false)(implicit ord: Ordering[T]): Option[Int] = {
-    def find(start: Int, end: Int, found: Option[Int]): Option[Int] = {
+  def binarySearchBound[N, T](mapping: N => T, first: N, last: N, needle: T, upper: Boolean, strict: Boolean = false)(implicit ev: Integral[N], ord: Ordering[T]): Option[N] = {
+    import ev._
+    val two = one + one
+    def find(start: N, end: N, found: Option[N]): Option[N] = {
       if(start <= end) {
-        val mid = (start + end) / 2
-        val value = array(mid)
+        val mid = (start + end) / two
+        val value = mapping(mid)
         if(upper) {
           val cmp = ord.compare(value, needle)
           if(cmp < 0 || !strict && cmp == 0) {
-            find(mid + 1, end, Some(mid))
+            find(mid + one, end, Some(mid))
           } else {
-            find(start, mid - 1, found)
+            find(start, mid - one, found)
           }
         } else {
           val cmp = ord.compare(value, needle)
           if(cmp > 0 || !strict && cmp == 0) {
-            find(start, mid - 1, Some(mid))
+            find(start, mid - one, Some(mid))
           } else {
-            find(mid + 1, end, found)
+            find(mid + one, end, found)
           }
         }
       } else {
         found
       }
     }
-    find(0, array.size - 1, None)
+    find(first, last, None)
   }
+
+  def binarySearchBoundArray[T](array: IndexedSeq[T], first: Int, last: Int, needle: T, upper: Boolean, strict: Boolean)(implicit ord: Ordering[T]): Option[Int] =
+    binarySearchBound(array, first, last, needle, upper, strict)
+
+  def binarySearchBoundArray[T](array: IndexedSeq[T], needle: T, upper: Boolean, strict: Boolean = false)(implicit ord: Ordering[T]): Option[Int] =
+    binarySearchBoundArray(array, 0, array.size - 1, needle, upper, strict)
 
 }
--- src/main/scala/competitivescala/utils/Vectors.scala
+++ src/main/scala/competitivescala/utils/Vectors.scala
@@ -2,61 +2,75 @@ package competitivescala.utils
 
 object Vectors {
 
-  case class Vec2(x: Int = 0, y: Int = 0) {
-    def pointwise(f: (Int, Int) => Int)(that: Vec2): Vec2 = Vec2(f(x, that.x), f(y, that.y))
-    def map(f: Int => Int): Vec2 = Vec2(f(x), f(y))
-    def +(that: Vec2): Vec2 = pointwise(_ + _)(that)
-    def -(that: Vec2): Vec2 = pointwise(_ - _)(that)
-    def *(that: Vec2): Vec2 = pointwise(_ * _)(that)
-    def /(that: Vec2): Vec2 = pointwise(_ / _)(that)
-    def *(v: Int): Vec2 = map(_ * v)
-    def /(v: Int): Vec2 = map(_ / v)
-    def unary_- : Vec2 = map(-_)
-    def euclidean: Int = x.abs + y.abs
-    def sum: Int = x + y
-    def dot(that: Vec2): Int = (this * that).sum
-    def sq: Int = this.dot(this)
-    def cross(that: Vec2): Int = x * that.y - y * that.x
-    def swap: Vec2 = Vec2(y, x)
+  case class Vec2[N](x: N, y: N)(implicit ev: Integral[N]) {
+    import ev._
+    def pointwise(f: (N, N) => N)(that: Vec2[N]): Vec2[N] = Vec2(f(x, that.x), f(y, that.y))
+    def map(f: N => N): Vec2[N] = Vec2(f(x), f(y))
+    def +(that: Vec2[N]): Vec2[N] = pointwise(_ + _)(that)
+    def -(that: Vec2[N]): Vec2[N] = pointwise(_ - _)(that)
+    def *(that: Vec2[N]): Vec2[N] = pointwise(_ * _)(that)
+    def /(that: Vec2[N]): Vec2[N] = pointwise(_ / _)(that)
+    def *(v: N): Vec2[N] = map(_ * v)
+    def /(v: N): Vec2[N] = map(_ / v)
+    def unary_- : Vec2[N] = map(-_)
+    def euclidean: N = x.abs + y.abs
+    def sum: N = x + y
+    def dot(that: Vec2[N]): N = (this * that).sum
+    def sq: N = this.dot(this)
+    def cross(that: Vec2[N]): N = x * that.y - y * that.x
+    def swap: Vec2[N] = Vec2(y, x)
     private def quadrant(i: Int): Int = ((i % 4) + 4) % 4
-    def clockwise(i: Int = 1): Vec2 = {
-      def iterate(i: Int, vec: Vec2): Vec2 = if (i > 0) iterate(i - 1, Vec2(vec.y, -vec.x)) else vec
+    def clockwise(i: Int = 1): Vec2[N] = {
+      def iterate(i: Int, vec: Vec2[N]): Vec2[N] = if (i > 0) iterate(i - 1, Vec2(vec.y, -vec.x)) else vec
       iterate(quadrant(i), this)
     }
-    def counterClockwise(i: Int = 1): Vec2 = clockwise(-i)
+    def counterClockwise(i: Int = 1): Vec2[N] = clockwise(-i)
   }
 
-  case class Vec3(x: Int = 0, y: Int = 0, z: Int = 0) {
-    def pointwise(f: (Int, Int) => Int)(that: Vec3): Vec3 = Vec3(f(x, that.x), f(y, that.y), f(z, that.z))
-    def map(f: Int => Int): Vec3 = Vec3(f(x), f(y), f(z))
-    def +(that: Vec3): Vec3 = pointwise(_ + _)(that)
-    def -(that: Vec3): Vec3 = pointwise(_ - _)(that)
-    def *(that: Vec3): Vec3 = pointwise(_ * _)(that)
-    def /(that: Vec3): Vec3 = pointwise(_ / _)(that)
-    def *(v: Int): Vec3 = map(_ * v)
-    def /(v: Int): Vec3 = map(_ / v)
-    def unary_- : Vec3 = map(-_)
-    def euclidean: Int = x.abs + y.abs + z.abs
-    def sum: Int = x + y + z
-    def dot(that: Vec3): Int = (this * that).sum
-    def sq: Int = this.dot(this)
-    def cross(that: Vec3): Vec3 = Vec3(y * that.z - z * that.y, z * that.x - x * that.z, x * that.y - y * that.x)
+  type Vec2Int = Vec2[Int]
+  def Vec2Int(x: Int = 0, y: Int = 0): Vec2Int = Vec2(x, y)
+
+
+  case class Vec3[N](x: N, y: N, z: N)(implicit ev: Integral[N]) {
+    import ev._
+    def pointwise(f: (N, N) => N)(that: Vec3[N]): Vec3[N] = Vec3(f(x, that.x), f(y, that.y), f(z, that.z))
+    def map(f: N => N): Vec3[N] = Vec3(f(x), f(y), f(z))
+    def +(that: Vec3[N]): Vec3[N] = pointwise(_ + _)(that)
+    def -(that: Vec3[N]): Vec3[N] = pointwise(_ - _)(that)
+    def *(that: Vec3[N]): Vec3[N] = pointwise(_ * _)(that)
+    def /(that: Vec3[N]): Vec3[N] = pointwise(_ / _)(that)
+    def *(v: N): Vec3[N] = map(_ * v)
+    def /(v: N): Vec3[N] = map(_ / v)
+    def unary_- : Vec3[N] = map(-_)
+    def euclidean: N = x.abs + y.abs + z.abs
+    def sum: N = x + y + z
+    def dot(that: Vec3[N]): N = (this * that).sum
+    def sq: N = this.dot(this)
+    def cross(that: Vec3[N]): Vec3[N] = Vec3(y * that.z - z * that.y, z * that.x - x * that.z, x * that.y - y * that.x)
   }
 
-  case class Vec4(x: Int = 0, y: Int = 0, z: Int = 0, w: Int = 0) {
-    def pointwise(f: (Int, Int) => Int)(that: Vec4): Vec4 = Vec4(f(x, that.x), f(y, that.y), f(z, that.z), f(w, that.w))
-    def map(f: Int => Int): Vec4 = Vec4(f(x), f(y), f(z), f(w))
-    def +(that: Vec4): Vec4 = pointwise(_ + _)(that)
-    def -(that: Vec4): Vec4 = pointwise(_ - _)(that)
-    def *(that: Vec4): Vec4 = pointwise(_ * _)(that)
-    def /(that: Vec4): Vec4 = pointwise(_ / _)(that)
-    def *(v: Int): Vec4 = map(_ * v)
-    def /(v: Int): Vec4 = map(_ / v)
-    def unary_- : Vec4 = map(-_)
-    def euclidean: Int = x.abs + y.abs + z.abs + w.abs
-    def sum: Int = x + y + z + w
-    def dot(that: Vec4): Int = (this * that).sum
-    def sq: Int = this.dot(this)
+  type Vec3Int = Vec3[Int]
+  def Vec3Int(x: Int = 0, y: Int = 0, z: Int = 0): Vec3Int = Vec3(x, y, z)
+
+
+  case class Vec4[N](x: N, y: N, z: N, w: N)(implicit ev: Integral[N]) {
+    import ev._
+    def pointwise(f: (N, N) => N)(that: Vec4[N]): Vec4[N] = Vec4(f(x, that.x), f(y, that.y), f(z, that.z), f(w, that.w))
+    def map(f: N => N): Vec4[N] = Vec4(f(x), f(y), f(z), f(w))
+    def +(that: Vec4[N]): Vec4[N] = pointwise(_ + _)(that)
+    def -(that: Vec4[N]): Vec4[N] = pointwise(_ - _)(that)
+    def *(that: Vec4[N]): Vec4[N] = pointwise(_ * _)(that)
+    def /(that: Vec4[N]): Vec4[N] = pointwise(_ / _)(that)
+    def *(v: N): Vec4[N] = map(_ * v)
+    def /(v: N): Vec4[N] = map(_ / v)
+    def unary_- : Vec4[N] = map(-_)
+    def euclidean: N = x.abs + y.abs + z.abs + w.abs
+    def sum: N = x + y + z + w
+    def dot(that: Vec4[N]): N = (this * that).sum
+    def sq: N = this.dot(this)
   }
+
+  type Vec4Int = Vec4[Int]
+  def Vec4Int(x: Int = 0, y: Int = 0, z: Int = 0, w: Int = 0): Vec4Int = Vec4(x, y, z, w)
 
 }
