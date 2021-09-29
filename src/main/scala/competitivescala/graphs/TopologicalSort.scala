d9721dd38928ada5550694b10d8734aca715c563
Florian Cassayre <florian.cassayre@gmail.com>
2021-09-29 22:36:09 +0200

    Tail recursive topological sort

--- src/main/scala/competitivescala/graphs/TopologicalSort.scala
+++ src/main/scala/competitivescala/graphs/TopologicalSort.scala
@@ -3,22 +3,26 @@ package competitivescala.graphs
 object TopologicalSort {
 
   // Complexity: O(E + V)
-  // Also bounded by call stack for extreme graphs!
   def topologicalSort[U](adjacency: Map[U, Set[U]]): Seq[U] = {
-    def dfs(u: U, marks: Map[U, Boolean], remaining: Set[U], sorted: Seq[U]): (Map[U, Boolean], Set[U], Seq[U]) = {
-      marks.get(u) match {
-        case Some(true) => (marks, remaining, sorted)
-        case Some(false) => throw new Exception // Cycle
-        case None =>
-          val (newMarks, newRemaining, newSorted) = adjacency.getOrElse(u, Set.empty[U]).foldLeft((marks + (u -> false), remaining - u, sorted)) {
-            case ((marks, remaining, sorted), v) => dfs(v, marks, remaining, sorted)
+    def dfs(stack: Seq[(U, Set[U])], marks: Map[U, Boolean], remaining: Set[U], sorted: Seq[U]): (Map[U, Boolean], Set[U], Seq[U]) = {
+      stack match {
+        case (u, adjacent) +: tail =>
+          adjacent.headOption match {
+            case Some(v) =>
+              marks.get(v) match {
+                case Some(false) => throw new Exception // Cycle
+                case Some(true) => dfs((u, adjacent.tail) +: tail, marks, remaining, sorted)
+                case None => dfs((v, adjacency.getOrElse(v, Set.empty[U])) +: (u, adjacent.tail) +: tail, marks + (v -> false), remaining, sorted)
+              }
+            case None => dfs(tail, marks + (u -> true), remaining - u, u +: sorted)
           }
-          (newMarks + (u -> true), newRemaining, u +: newSorted)
+        case _ => (marks, remaining, sorted)
       }
     }
     def iterate(marks: Map[U, Boolean], remaining: Set[U], sorted: Seq[U]): Seq[U] = {
       if(remaining.nonEmpty) {
-        val (newMarks, newRemaining, newSorted) = dfs(remaining.head, marks, remaining, sorted)
+        val u = remaining.head
+        val (newMarks, newRemaining, newSorted) = dfs(Seq((u, adjacency.getOrElse(u, Set.empty[U]))), marks + (u -> false), remaining - u, sorted)
         iterate(newMarks, newRemaining, newSorted)
       } else {
         sorted
--- src/test/scala/competitivescala/graphs/TopologicalSortTest.scala
+++ src/test/scala/competitivescala/graphs/TopologicalSortTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TopologicalSortTest extends AnyFunSuite {
 
-  test("topological sort") {
+  test("topological sort on small graphs") {
     assert(
       topologicalSort(Map('a' -> Set('b', 'c'), 'b' -> Set('c'), 'c' -> Set.empty[Char])) ==
         Seq('a', 'b', 'c')
@@ -15,6 +15,25 @@ class TopologicalSortTest extends AnyFunSuite {
       topologicalSort(Map('a' -> Set('d', 'e'), 'b' -> Set('a', 'd'), 'c' -> Set('e'), 'd' -> Set('c'), 'e' -> Set.empty[Char])) ==
         Seq('b', 'a', 'd', 'c', 'e')
     )
+  }
+
+  test("topological sort on an extreme deep and shallow graph") {
+    val nodes = (0 until 10000).map(_.toString)
+    val graph = nodes.zip(nodes.tail).map { case (a, b) => a -> Set(b) }.toMap
+
+    assert(topologicalSort(graph) == nodes)
+  }
+
+  test("topological sort on a singleton graph") {
+    assert(topologicalSort(Map('a' -> Set('b'))) == Seq('a', 'b'))
+  }
+
+  test("topological sort on an empty graph") {
+    assert(topologicalSort(Map.empty) == Seq.empty)
+  }
+
+  test("topological sort on a cyclic graph (illegal)") {
+    assertThrows[Exception](topologicalSort(Map('a' -> Set('b'), 'b' -> Set('c'), 'c' -> Set('a'))))
   }
 
 }
